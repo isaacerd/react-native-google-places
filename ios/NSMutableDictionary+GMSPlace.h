41139b88dffef0adc51b917bec6b79c21ce822e9
Isaac Ramirez <1125167-isaacerd@users.noreply.gitlab.com>
2024-11-15 12:27:47 -0400

    fix for android SDK 34

--- ios/NSMutableDictionary+GMSPlace.h
+++ ios/NSMutableDictionary+GMSPlace.h
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <GooglePlaces/GooglePlaces.h>
-
+#import <GoogleMapsBase/GoogleMapsBase.h>
 @interface NSMutableDictionary (GMSPlace)
 
 + (instancetype)dictionaryWithGMSPlace:(GMSPlace*)place;
